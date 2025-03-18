5d1842edefecc894d99e9af3ddce2b59faf6cb30
As@126548 <Itspantheraleo>
2025-03-18 22:42:33 +0530

    Created frontend of calculator using django

--- djangoprojects/calculatorapp/views.py
+++ djangoprojects/calculatorapp/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,JsonResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Server Started")
+    return render(request,'index.html')
+
+def submitquery(request):
+    q = request.GET['query']
+    return HttpResponse(q)
+
